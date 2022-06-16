9558b5c7377fcb9d6ccb81453e40b89c66dc7c90
klumiere <105880255+klumiere@users.noreply.github.com>
2022-06-16 12:12:57 +0200

    Add files via upload

---
+++ kamely_files/aaa.js
@@ -0,0 +1,30 @@
+queryString=window.location.search; 
+console.log(queryString);// récupère tout ce qu'il y a écrit après le ?
+
+
+const urlParams=new URLSearchParams (queryString);
+
+//const snap=urlParams.get('snapshot-url') // récupère la 1ère variable après le ?
+//console.log(snap);
+
+//const type=urlParams.get('snapshot-url-type') // récupère la 2ème variable après le ? qui est juste après le &
+//console.log(type);
+
+const
+
+keys=urlParams.keys(),
+values=urlParams.values(),
+entries=urlParams.entries();
+
+//for (const key of keys) console.log(key);
+
+//for(const value of values) console.log(value);
+
+for(const entry of entries){
+
+  console.log(`${entry[0]}`);
+  var code_pdb=entry[0] //stocke la valeur du code pdb dans la variable cod_pdb
+  
+ }
+ 
+ console.log(code_pdb)
