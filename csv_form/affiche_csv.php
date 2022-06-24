f9c3cd0f62cb958e2f216921eaf40ea27e472e1b
klumiere <105880255+klumiere@users.noreply.github.com>
2022-06-25 00:04:10 +0200

    Update affiche_csv.php

--- csv_form/affiche_csv.php
+++ csv_form/affiche_csv.php
@@ -7,19 +7,19 @@
 <body>
 
 <?php
-   $taille_min_seq = $_GET['taille_min_seq'];
-   $taille_max_seq = $_GET['taille_max_seq'];
-   $resol_max= $_GET ['resol_max'];
+   $taille_min_seq = $_GET['taille_min_seq']; // récupère la taille min entrée par l'utilisateur
+   $taille_max_seq = $_GET['taille_max_seq'];// récupère la taille max entrée par l'utilisateur
+   $resol_max= $_GET ['resol_max']; // récupère la résolution max entrée par l'utilisateur
    
-  $nom_dossier = 'RNANet_fichiers';
-  $dossier = opendir($nom_dossier);
+  $nom_dossier = 'RNANet_fichiers';  
+  $dossier = opendir($nom_dossier); // ouvre les fichiers du dossier RNANet_fichiers contenant les fichiers plats du site RNANet
 
 while($fichier = readdir($dossier))
 {
-    $lines = count(file($nom_dossier."/".$fichier));
-    $lignes=$lines-1; //la longueur de la sequence fait le nb de lignes du fichier -1
+    $lines = count(file($nom_dossier."/".$fichier)); //compte le nombre de lignes pour chaque fichier txt du dossier
+    $lignes=$lines-1; //la longueur de la sequence fait le nb de lignes du fichier -1 car la séquence est lu de haut en bas
     
-if (($lignes>$taille_min_seq) && ($lignes<$taille_max_seq)){
+if (($lignes>$taille_min_seq) && ($lignes<$taille_max_seq)){ // si la longueur de la séquence est comprise dans l'intervalle demandé on affiche le nom du fichier suivi de sa longueur
 echo $fichier. "  Le nombre de lignes est". $lignes . "<br>";
 }
 
